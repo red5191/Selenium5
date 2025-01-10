d4ee45a960ac97effe6a56878795708bd727cc64
red5191 <the_red51@outlook.com>
2025-01-10 14:36:09 +0300

    dz5

--- selenium_python_chrome.py
+++ selenium_python_chrome.py
@@ -3,20 +3,34 @@
 # pip install selenium (Устанавливаем библиотеку selenium)
 # pip install webdriver-manager (Устанавливаем webdriver-manager)
 
+# импортируем необходимые библиотеки и элементы
 from selenium.webdriver.common.by import By
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 
+# создаем и настраиваем экземпляр driver класса webdriver
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach",True)
+options.add_argument('--headless')
 driver = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
+
+# создаем переменную содержащую базовую ссылку и открываем её с помощью созданного ранее driver
 base_url = 'https://www.saucedemo.com/'
 driver.get(base_url)
 driver.maximize_window()
 
+# вводим логин
 user_name = driver.find_element(By.XPATH, "//*[@id='user-name']")
 user_name.send_keys('standard_user')
+print('Введен логин')
 
+# вводим пароль
 password = driver.find_element(By.NAME , "password")
 password.send_keys('secret_sauce')
+print("Введен пароль")
+
+# нажимаем кнопку авторизации
+login_button = driver.find_element(By.ID, 'login-button')
+login_button.click()
+print("Произведен \"клик\" по кнопке")
